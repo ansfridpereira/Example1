eeb3a30c9e11417a1a414801c4e36cc5ef0640c7
ansfridpereira <89774958+ansfridpereira@users.noreply.github.com>
2021-10-22 15:18:44 +0530

    Initial commit

---
+++ src/Example1.java
@@ -0,0 +1,21 @@
+import kotlin.reflect.jvm.internal.impl.util.collectionUtils.ScopeUtilsKt;
+
+class Example1 {
+    public static void main(String args[])
+    {
+    int num, num1,num2,num3,num4,num5;
+    num =200;
+     System.out.println("The number is :" +num);
+    num1=num*2;
+    System.out.println("The value of num*2 is " +num1);
+        num2=num*3;
+        System.out.println("The value of num*3 is " +num2);
+        num3=num*4;
+        System.out.println("The value of num*4 is " +num3);
+        num4=num*5;
+        System.out.println("The value of num*5 is " +num4);
+        num5=num*6;
+        System.out.println("The value of num*6 is " +num5);
+
+}
+}
